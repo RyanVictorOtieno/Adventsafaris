097062862376f2f1ee6d3cdb1ac60ed9cd5014d9
Victor Otieno <victorotieno069@gmail.com>
2020-04-10 01:52:46 +0300

    Mpesa API

--- register_url.php
+++ register_url.php
@@ -1,12 +1,36 @@
 <?php
+	
+$consumerKey = 'ATCAOqYt4DGapujfaWfGGpONhh2wKXNO'; //Fill with your app Consumer Key
+	$consumerSecret = '5HCQCAn056dEOMKV'; // Fill with your app Secret
+
+	$headers = ['Content-Type:application/json; charset=utf8'];
+
+	$url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($curl, CURLOPT_HEADER, FALSE);
+	curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+	$result = curl_exec($curl);
+	$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+	$result = json_decode($result);
+
+	$access_token = $result->access_token;
+
+	echo $access_token;
+	
+
+
+
 	$url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
 
-	$access_token = ''; // check the mpesa_accesstoken.php file for this. No need to writing a new file here, just combine the code as in the tutorial.
-	$shortCode = ''; // provide the short code obtained from your test credentials
+	// $access_token = ''; // check the mpesa_accesstoken.php file for this. No need to writing a new file here, just combine the code as in the tutorial.
+	$shortCode = '601443'; // provide the short code obtained from your test credentials
 
 	/* This two files are provided in the project. */
-	$confirmationUrl = ''; // path to your confirmation url. can be IP address that is publicly accessible or a url
-	$validationUrl = ''; // path to your validation url. can be IP address that is publicly accessible or a url
+	$confirmationUrl = 'https://ryanvictorotieno.github.io/Adventsafaris/confirmation_url.php'; // path to your confirmation url. can be IP address that is publicly accessible or a url
+	$validationUrl = 'https://ryanvictorotieno.github.io/Adventsafaris/validation.php'; // path to your validation url. can be IP address that is publicly accessible or a url
 
 
 

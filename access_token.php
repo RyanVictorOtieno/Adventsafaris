545d592a2e2755758b546953587f73fa2134bed9
Victor Otieno <victorotieno069@gmail.com>
2020-04-10 00:50:43 +0300

    mpesa inegration

---
+++ access_token.php
@@ -0,0 +1,25 @@
+<?php 
+
+	$consumerKey = 'ATCAOqYt4DGapujfaWfGGpONhh2wKXNO'; //Fill with your app Consumer Key
+	$consumerSecret = '5HCQCAn056dEOMKV'; // Fill with your app Secret
+
+	$headers = ['Content-Type:application/json; charset=utf8'];
+
+	$url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($curl, CURLOPT_HEADER, FALSE);
+	curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+	$result = curl_exec($curl);
+	$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+	$result = json_decode($result);
+
+	$access_token = $result->access_token;
+
+	echo $access_token;
+	
+	curl_close($curl);
+
+?>
